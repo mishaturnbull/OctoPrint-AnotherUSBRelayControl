8831829d10473a9c13fe6c042d12f26ae1feeaaf
Misha Turnbull <mishaturnbull@gmail.com>
2025-05-11 14:49:39 -0500

    Align setup.py config and package directory naming
    Can you tell this is my first octoprint plugin??

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@
 ### Do not forget to adjust the following variables to your own plugin.
 
 # The plugin's identifier, has to be unique
-plugin_identifier = "dumberusbrelaycontrol"
+plugin_identifier = "anotherusbrelaycontrol"
 
 # The plugin's python package, should be "octoprint_<plugin identifier>", has to be unique
-plugin_package = "octoprint_dumberusbrelaycontrol"
+plugin_package = "octoprint_anotherusbrelaycontrol"
 
 # The plugin's human readable name. Can be overwritten within OctoPrint's internal data via __plugin_name__ in the
 # plugin module
